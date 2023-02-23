d757953c35b8d1a4cce6a736d24de28a2c9f6266
GreatGrogrodor <george.daniel.broughton@gmail.com>
2023-02-23 06:25:10 +0000

    Adjusted install.ps1 for docker install

--- Install.ps1
+++ Install.ps1
@@ -1,14 +1,23 @@
 Set-Location -Path $PSScriptRoot
 
-Write-Host 'Downloading Dependencies (might take a while)'
+Write-Host "Downloading Weedie's modpack"
     Invoke-WebRequest -Uri 'https://edge.forgecdn.net/files/4301/113/Version%20108%20server%20files.zip' -OutFile './WWNGP-Server.zip'
 
+if (!(Get-WinGetPackage -ID "Docker.DockerDesktop")) { 
+    Write-Host "Installing Docker Desktop"
+        Install-WinGetPackage -ID "Docker.DockerDesktop"
+} else {
+    Write-Host "Docker Desktop is already installed and will not be downloaded."
+}
+
 Write-Host 'Installing Mods'
-    if (!(Test-Path -Path ".\Minecraft-Data")) { New-Item -Path ".\Minecraft-Data" -ItemType Directory }
+    if (!(Test-Path -Path ".\Minecraft-Data")) {
+        Write-Host 'Creating new folder to install game files into.'
+        New-Item -Path ".\Minecraft-Data" -ItemType Directory
+    }
     Expand-Archive -Path '.\WWNGP-Server.zip' -DestinationPath '.\Minecraft-Data'
     Remove-Item -Path '.\WWNGP-Server.zip'
 
 docker-compose up -d
 
 Write-Host ''
-Write-Host 'Done'
